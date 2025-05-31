c89fd3727fca560c7e259c5547f707aee8ac780c
Hinana-4 <1684900324@qq.com>
2025-05-31 19:07:20 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import CheckInInterface from './CheckInInterface'; // 导入刚刚创建的组件
+
+function App() {
+  return (
+    <div className="App">
+      <CheckInInterface />
+    </div>
+  );
+}
+
+export default App;
